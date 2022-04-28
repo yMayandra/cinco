9d84afbd94a37e36f5d54e08a90323f52edfe5aa
mayandra.vmota <mayandra.vmota@SBC0505221W10-1>
2022-04-28 17:30:11 -0300

    Adicionar arquivos de projeto.

---
+++ cinco/View/Main.Designer.cs
@@ -0,0 +1,289 @@
+﻿
+namespace cinco.View
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCompra = new System.Windows.Forms.Label();
+            this.lblDinheiro = new System.Windows.Forms.Label();
+            this.lblMoedas100 = new System.Windows.Forms.Label();
+            this.lblTroco = new System.Windows.Forms.Label();
+            this.lblResultado100 = new System.Windows.Forms.Label();
+            this.lblResultado050 = new System.Windows.Forms.Label();
+            this.lblResultado025 = new System.Windows.Forms.Label();
+            this.lblMoedas050 = new System.Windows.Forms.Label();
+            this.lblMoedas025 = new System.Windows.Forms.Label();
+            this.lblMoedas010 = new System.Windows.Forms.Label();
+            this.lblMoedas001 = new System.Windows.Forms.Label();
+            this.lblResultado005 = new System.Windows.Forms.Label();
+            this.lblResultado010 = new System.Windows.Forms.Label();
+            this.lblMoedas = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblResultado001 = new System.Windows.Forms.Label();
+            this.lblMoedas005 = new System.Windows.Forms.Label();
+            this.tbxCompra = new System.Windows.Forms.TextBox();
+            this.tbxPago = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCompra
+            // 
+            this.lblCompra.AutoSize = true;
+            this.lblCompra.Location = new System.Drawing.Point(36, 71);
+            this.lblCompra.Name = "lblCompra";
+            this.lblCompra.Size = new System.Drawing.Size(93, 15);
+            this.lblCompra.TabIndex = 0;
+            this.lblCompra.Text = "Valor da compra";
+            this.lblCompra.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblDinheiro
+            // 
+            this.lblDinheiro.AutoSize = true;
+            this.lblDinheiro.Location = new System.Drawing.Point(36, 104);
+            this.lblDinheiro.Name = "lblDinheiro";
+            this.lblDinheiro.Size = new System.Drawing.Size(63, 15);
+            this.lblDinheiro.TabIndex = 1;
+            this.lblDinheiro.Text = "Valor pago";
+            // 
+            // lblMoedas100
+            // 
+            this.lblMoedas100.AutoSize = true;
+            this.lblMoedas100.Location = new System.Drawing.Point(435, 91);
+            this.lblMoedas100.Name = "lblMoedas100";
+            this.lblMoedas100.Size = new System.Drawing.Size(102, 15);
+            this.lblMoedas100.TabIndex = 2;
+            this.lblMoedas100.Text = "Moedas de R$1,00";
+            // 
+            // lblTroco
+            // 
+            this.lblTroco.AutoSize = true;
+            this.lblTroco.Location = new System.Drawing.Point(36, 133);
+            this.lblTroco.Name = "lblTroco";
+            this.lblTroco.Size = new System.Drawing.Size(81, 15);
+            this.lblTroco.TabIndex = 3;
+            this.lblTroco.Text = "Valor do troco";
+            // 
+            // lblResultado100
+            // 
+            this.lblResultado100.AutoSize = true;
+            this.lblResultado100.Location = new System.Drawing.Point(579, 91);
+            this.lblResultado100.Name = "lblResultado100";
+            this.lblResultado100.Size = new System.Drawing.Size(13, 15);
+            this.lblResultado100.TabIndex = 4;
+            this.lblResultado100.Text = "0";
+            // 
+            // lblResultado050
+            // 
+            this.lblResultado050.AutoSize = true;
+            this.lblResultado050.Location = new System.Drawing.Point(579, 113);
+            this.lblResultado050.Name = "lblResultado050";
+            this.lblResultado050.Size = new System.Drawing.Size(13, 15);
+            this.lblResultado050.TabIndex = 5;
+            this.lblResultado050.Text = "0";
+            // 
+            // lblResultado025
+            // 
+            this.lblResultado025.AutoSize = true;
+            this.lblResultado025.Location = new System.Drawing.Point(579, 141);
+            this.lblResultado025.Name = "lblResultado025";
+            this.lblResultado025.Size = new System.Drawing.Size(13, 15);
+            this.lblResultado025.TabIndex = 6;
+            this.lblResultado025.Text = "0";
+            // 
+            // lblMoedas050
+            // 
+            this.lblMoedas050.AutoSize = true;
+            this.lblMoedas050.Location = new System.Drawing.Point(435, 113);
+            this.lblMoedas050.Name = "lblMoedas050";
+            this.lblMoedas050.Size = new System.Drawing.Size(105, 15);
+            this.lblMoedas050.TabIndex = 7;
+            this.lblMoedas050.Text = "Moedas de R$ 0,50";
+            // 
+            // lblMoedas025
+            // 
+            this.lblMoedas025.AutoSize = true;
+            this.lblMoedas025.Location = new System.Drawing.Point(435, 141);
+            this.lblMoedas025.Name = "lblMoedas025";
+            this.lblMoedas025.Size = new System.Drawing.Size(102, 15);
+            this.lblMoedas025.TabIndex = 8;
+            this.lblMoedas025.Text = "Moedas de R$0,25";
+            // 
+            // lblMoedas010
+            // 
+            this.lblMoedas010.AutoSize = true;
+            this.lblMoedas010.Location = new System.Drawing.Point(435, 168);
+            this.lblMoedas010.Name = "lblMoedas010";
+            this.lblMoedas010.Size = new System.Drawing.Size(102, 15);
+            this.lblMoedas010.TabIndex = 9;
+            this.lblMoedas010.Text = "Moedas de R$0,10";
+            // 
+            // lblMoedas001
+            // 
+            this.lblMoedas001.AutoSize = true;
+            this.lblMoedas001.Location = new System.Drawing.Point(435, 214);
+            this.lblMoedas001.Name = "lblMoedas001";
+            this.lblMoedas001.Size = new System.Drawing.Size(102, 15);
+            this.lblMoedas001.TabIndex = 10;
+            this.lblMoedas001.Text = "Moedas de R$0,01";
+            // 
+            // lblResultado005
+            // 
+            this.lblResultado005.AutoSize = true;
+            this.lblResultado005.Location = new System.Drawing.Point(579, 191);
+            this.lblResultado005.Name = "lblResultado005";
+            this.lblResultado005.Size = new System.Drawing.Size(13, 15);
+            this.lblResultado005.TabIndex = 11;
+            this.lblResultado005.Text = "0";
+            // 
+            // lblResultado010
+            // 
+            this.lblResultado010.AutoSize = true;
+            this.lblResultado010.Location = new System.Drawing.Point(579, 168);
+            this.lblResultado010.Name = "lblResultado010";
+            this.lblResultado010.Size = new System.Drawing.Size(13, 15);
+            this.lblResultado010.TabIndex = 12;
+            this.lblResultado010.Text = "0";
+            // 
+            // lblMoedas
+            // 
+            this.lblMoedas.AutoSize = true;
+            this.lblMoedas.Location = new System.Drawing.Point(434, 63);
+            this.lblMoedas.Name = "lblMoedas";
+            this.lblMoedas.Size = new System.Drawing.Size(107, 15);
+            this.lblMoedas.TabIndex = 13;
+            this.lblMoedas.Text = "Moedas para Troco";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(123, 133);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(13, 15);
+            this.lblResultado.TabIndex = 14;
+            this.lblResultado.Text = "0";
+            this.lblResultado.Click += new System.EventHandler(this.label15_Click);
+            // 
+            // lblResultado001
+            // 
+            this.lblResultado001.AutoSize = true;
+            this.lblResultado001.Location = new System.Drawing.Point(579, 214);
+            this.lblResultado001.Name = "lblResultado001";
+            this.lblResultado001.Size = new System.Drawing.Size(13, 15);
+            this.lblResultado001.TabIndex = 15;
+            this.lblResultado001.Text = "0";
+            // 
+            // lblMoedas005
+            // 
+            this.lblMoedas005.AutoSize = true;
+            this.lblMoedas005.Location = new System.Drawing.Point(435, 191);
+            this.lblMoedas005.Name = "lblMoedas005";
+            this.lblMoedas005.Size = new System.Drawing.Size(102, 15);
+            this.lblMoedas005.TabIndex = 16;
+            this.lblMoedas005.Text = "Moedas de R$0,05";
+            // 
+            // tbxCompra
+            // 
+            this.tbxCompra.Location = new System.Drawing.Point(141, 63);
+            this.tbxCompra.Name = "tbxCompra";
+            this.tbxCompra.Size = new System.Drawing.Size(100, 23);
+            this.tbxCompra.TabIndex = 17;
+            // 
+            // tbxPago
+            // 
+            this.tbxPago.Location = new System.Drawing.Point(141, 96);
+            this.tbxPago.Name = "tbxPago";
+            this.tbxPago.Size = new System.Drawing.Size(100, 23);
+            this.tbxPago.TabIndex = 18;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.AccessibleDescription = "";
+            this.btnCalcular.Location = new System.Drawing.Point(166, 160);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 19;
+            this.btnCalcular.Text = "Calcular ";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.tbxPago);
+            this.Controls.Add(this.tbxCompra);
+            this.Controls.Add(this.lblMoedas005);
+            this.Controls.Add(this.lblResultado001);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblMoedas);
+            this.Controls.Add(this.lblResultado010);
+            this.Controls.Add(this.lblResultado005);
+            this.Controls.Add(this.lblMoedas001);
+            this.Controls.Add(this.lblMoedas010);
+            this.Controls.Add(this.lblMoedas025);
+            this.Controls.Add(this.lblMoedas050);
+            this.Controls.Add(this.lblResultado025);
+            this.Controls.Add(this.lblResultado050);
+            this.Controls.Add(this.lblResultado100);
+            this.Controls.Add(this.lblTroco);
+            this.Controls.Add(this.lblMoedas100);
+            this.Controls.Add(this.lblDinheiro);
+            this.Controls.Add(this.lblCompra);
+            this.Name = "Main";
+            this.Text = "Sistema de Troco";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCompra;
+        private System.Windows.Forms.Label lblDinheiro;
+        private System.Windows.Forms.Label lblMoedas100;
+        private System.Windows.Forms.Label lblTroco;
+        private System.Windows.Forms.Label lblResultado100;
+        private System.Windows.Forms.Label lblResultado050;
+        private System.Windows.Forms.Label lblResultado025;
+        private System.Windows.Forms.Label lblMoedas050;
+        private System.Windows.Forms.Label lblMoedas025;
+        private System.Windows.Forms.Label lblMoedas010;
+        private System.Windows.Forms.Label lblMoedas001;
+        private System.Windows.Forms.Label lblResultado005;
+        private System.Windows.Forms.Label lblResultado010;
+        private System.Windows.Forms.Label lblMoedas;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblResultado001;
+        private System.Windows.Forms.Label lblMoedas005;
+        private System.Windows.Forms.TextBox tbxCompra;
+        private System.Windows.Forms.TextBox tbxPago;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
---
+++ cinco/View/Main.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cinco.View
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            decimal valorCompra = 0;
+            decimal valorPago = 0;
+            decimal valorTroco = 0;
+
+
+
+            valorCompra = Convert.ToDecimal( tbxCompra.Text);
+            valorPago = Convert.ToDecimal(tbxPago.Text);
+            valorTroco = valorPago - valorCompra;
+
+            lblResultado.Text = valorTroco.ToString();
+
+            int moeda100 = (int) valorTroco / 1;
+            lblResultado100.Text = moeda100.ToString();
+
+            int moeda050 = (int)(valorTroco / 0.50m);
+            lblResultado050.Text = moeda050.ToString();
+
+            int moeda025 = (int)(valorTroco / 0.25m);
+            lblResultado025.Text = moeda025.ToString();
+
+            int moeda010 = (int)(valorTroco / 0.10m);
+            lblResultado010.Text = moeda010.ToString();
+
+            int moeda005 = (int)(valorTroco / 0.05m);
+            lblResultado005.Text = moeda005.ToString();
+
+            int moeda001 = (int)(valorTroco / 0.01m);
+            lblResultado001.Text = moeda001.ToString();
+
+        }
+    }
+}
